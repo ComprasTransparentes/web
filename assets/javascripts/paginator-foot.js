29d22ff4cc51f97e0b1e547704db109812a7a55e
malamen <d.chavezescobar@gmail.com>
2015-09-30 20:08:02 -0300

    .

--- assets/javascripts/paginator-foot.js
+++ assets/javascripts/paginator-foot.js
@@ -10,9 +10,9 @@ var options1 = {
                 
                 $.getJSON( url, function( data ) {
                 	console.log(data);
-                    $('.search-result-item').attr("style","display: none");
+                    $('#tipo1').find("[type='lic']").attr("style","display: none");
 					for(var b=0; b< data.licitaciones.length;b++){
-                        $('#tipo1').find("[code='"+b+"']").attr("style","display: block");
+                        $('#tipo1').find("[code='"+b+"']").filter("[type='lic']").attr("style","display: block");
 	                	$('.search-result-item .collapsable-text').find("[namelinklic='"+b+"']").text(data.licitaciones[b].nombre);
                         $('.search-result-item .collapsable-text').find("[namelinklic='"+b+"']").attr("href","/file?type=licitacion&code="+data.licitaciones[b].codigo);
                         $('.search-result-item').find("[nameorglic='"+b+"']").text(data.licitaciones[b].organismo.nombre);
@@ -29,14 +29,44 @@ $('#pag1').bootstrapPaginator(options1);
 
 var options3 = {
             currentPage: 1,
-            totalPages: Math.ceil(n_org/10)
+            totalPages: Math.ceil(n_org/maxitems),
+            onPageChanged: function(e,oldPage,newPage){
+                var url = "http://"+host+"/organismo/?q="+ele+"&pagina="+newPage;
+                
+                $.getJSON( url, function( data ) {
+                    console.log(data);
+                    $('#tipo3').find("[type='org']").attr("style","display: none");
+                    for(var b=0; b< data.organismos.length;b++){
+                        $('#tipo3').find("[code='"+b+"']").filter("[type='org']").attr("style","display: block");
+                        $('.search-result-item .collapsable-text').find("[namelinkorg='"+b+"']").text(data.organismos[b].nombre);
+                        $('.search-result-item .collapsable-text').find("[namelinkorg='"+b+"']").attr("href","/file?type=organismo&code="+data.organismos[b].codigo);
+                        $('.search-result-item').find("[namecat='"+b+"']").text(data.organismos[b].categoria);                    
+                    }
+                });
+
+            }
         }
 
 $('#pag3').bootstrapPaginator(options3);
 
 var options4 = {
             currentPage: 1,
-            totalPages: Math.ceil(n_pro/10)
+            totalPages: Math.ceil(n_pro/maxitems),
+            onPageChanged: function(e,oldPage,newPage){
+                var url = "http://"+host+"/proveedor/?q="+ele+"&pagina="+newPage;
+                
+                $.getJSON( url, function( data ) {
+                    console.log(data);
+                    $('#tipo4').find("[type='pro']").attr("style","display: none");
+                    for(var b=0; b< data.proveedores.length;b++){
+                        $('#tipo4').find("[code='"+b+"']").filter("[type='pro']").attr("style","display: block");
+                        $('.search-result-item .collapsable-text').find("[namelinkpro='"+b+"']").text(data.proveedores[b].nombre);
+                        $('.search-result-item .collapsable-text').find("[namelinkorg='"+b+"']").attr("href","/file?type=proveedores&code="+data.proveedores[b].id);
+                        $('.search-result-item').find("[rutpro='"+b+"']").text(data.proveedores[b].rut);                    
+                    }
+                });
+
+            }
         }
 
 $('#pag4').bootstrapPaginator(options4);
