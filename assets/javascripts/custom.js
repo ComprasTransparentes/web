b2943272f69dd065957cdf27552578b245cf2d67
Osman Cea <osman.cea@gmail.com>
2015-09-29 22:12:24 -0300

    gatito

--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -23,9 +23,14 @@ $(document).ready(function() {
 
 	$('.numformat').each( function() {
 
+		if(parseInt($(this).text()) > 10000000) {
+			console.log("gatito");
+		}
+
 		$(this).text($(this).text().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1."));
 
 	});
+
 
 
 
