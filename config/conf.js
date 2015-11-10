44e0b85494006caf8abcbf744c89854bf826c4fd
DVarela <dvarela@zoohash.com>
2015-11-10 17:48:49 -0300

    Fix fileOrg - bugs

--- config/conf.js
+++ config/conf.js
@@ -1,10 +1,10 @@
 var conf = {};
 
-conf.ip = '192.168.100.10';
-//conf.ip = 'comprastransparentes.cl';
+//conf.ip = '192.168.100.10';
+conf.ip = 'comprastransparentes.cl';
 //conf.ip = 'hacklab.errdd.com';
-conf.port = 8000;
-//conf.port = 8002;
+//conf.port = 8000;
+conf.port = 8003;
 conf.host = "http://" + conf.ip + ":" + conf.port;	
 
 module.exports = conf;
