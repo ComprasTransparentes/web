6246c9cabb522221717ea866da2cd7522225c573
DVarela <dvarela@zoohash.com>
2015-11-25 22:17:25 -0300

    Fix Search

--- config/conf.js
+++ config/conf.js
@@ -2,13 +2,13 @@ var conf = {};
 
 
 //conf.ip = '127.0.0.1';
-conf.ip = '192.168.100.11';
+//conf.ip = '192.168.100.11';
 //conf.ip = 'dev.comprastransparentes.cl';
-
+conf.ip = 'cctt.ngrok.io'
 
 //conf.ip = 'hacklab.errdd.com';
 //conf.port = 8000;
-conf.port = 8000;
+conf.port = 80;
 conf.host = "http://" + conf.ip + ":" + conf.port;	
 
 module.exports = conf;
