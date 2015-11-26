fb10f421a6f1695b3c7099df9334473dc2ad77ec
dvarelae <dvarela@zoohash.com>
2015-11-26 05:09:58 -0200

    Fix Code Links

--- assets/javascripts/paginator-foot.js
+++ assets/javascripts/paginator-foot.js
@@ -15,7 +15,7 @@ var options1 = {
 					for(var b=0; b< data.licitaciones.length;b++){
                         $('#tipo1').find("[code='"+b+"']").filter("[type='lic']").attr("style","display: block");
 	                	$('.search-result-item .collapsable-text').find("[namelinklic='"+b+"']").text(data.licitaciones[b].nombre);
-                        $('.search-result-item .collapsable-text').find("[namelinklic='"+b+"']").attr("href","/file?type=licitacion&code="+data.licitaciones[b].codigo);
+                        $('.search-result-item .collapsable-text').find("[namelinklic='"+b+"']").attr("href","/file?type=licitacion&code="+data.licitaciones[b].id);
                         $('.search-result-item').find("[nameorglic='"+b+"']").text(data.licitaciones[b].organismo.nombre);
                         $('.search-result-item').find("[linkorglic='"+b+"']").attr("href","/file?type=organismo&code="+data.licitaciones[b].organismo.id);
                         $('.search-result-item').find("[deslic='"+b+"']").text(data.licitaciones[b].descripcion);
@@ -40,7 +40,7 @@ var options3 = {
                     for(var b=0; b< data.organismos.length;b++){
                         $('#tipo3').find("[code='"+b+"']").filter("[type='org']").attr("style","display: block");
                         $('.search-result-item .collapsable-text').find("[namelinkorg='"+b+"']").text(data.organismos[b].nombre);
-                        $('.search-result-item .collapsable-text').find("[namelinkorg='"+b+"']").attr("href","/file?type=organismo&code="+data.organismos[b].codigo);
+                        $('.search-result-item .collapsable-text').find("[namelinkorg='"+b+"']").attr("href","/file?type=organismo&code="+data.organismos[b].id);
                         $('.search-result-item').find("[namecat='"+b+"']").text(data.organismos[b].categoria);                    
                     }
                 });
