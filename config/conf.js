5cc5ab5c05cdb6d1648fc49d9c65b85d0f9fed92
Luis Benitez <lbenitez000@gmail.com>
2015-11-27 19:55:03 -0300

    fix boton de acta y botond de embed

--- config/conf.js
+++ config/conf.js
@@ -4,11 +4,9 @@ var conf = {};
 conf.ip = 'api.comprastransparentes.cl';
 //conf.ip = '192.168.100.11';
 // conf.ip = 'cctt.ngrok.io';
-//conf.ip = 'dev.comprastransparentes.cl';
 
-//conf.ip = 'hacklab.errdd.com';
 conf.port = 80;
-//conf.port = 80;
+//conf.port = 8000;
 conf.host = "http://" + conf.ip + ":" + conf.port;	
 
 module.exports = conf;
